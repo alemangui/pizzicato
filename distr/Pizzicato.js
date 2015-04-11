cac8da4f677b25a606b105d56e3e720f23c6afb9
Alejandro Mantecon Guillen <alemangui@gmail.com>
2015-04-11 15:43:30 +0200

    Changes the way the delay effect works to allow modifying parameters while it's being executed.

--- distr/Pizzicato.js
+++ distr/Pizzicato.js
@@ -182,7 +182,7 @@
 	Pizzicato.Effects = {};
 
 	Pizzicato.Effects.Delay = function(options) {
-		
+	
 		this.options = options || {};
 	
 		var defaults = {
@@ -193,45 +193,160 @@
 	
 		for (var key in defaults)
 			this.options[key] = typeof this.options[key] === 'undefined' ? defaults[key] : this.options[key];
+	
+		this.inputGainNode = Pizzicato.context.createGain();
+		this.dryGainNode = Pizzicato.context.createGain();
+		this.wetGainNode = Pizzicato.context.createGain();
+		this.outputGainNode = Pizzicato.context.createGain();
+	
+		this.adjustMix();
+	
+		this.inputGainNode.connect(this.dryGainNode);
+		this.dryGainNode.connect(this.outputGainNode);
+		this.wetGainNode.connect(this.outputGainNode);
+	
+		this.createDelayLoop();	
 	};
 	
-	Pizzicato.Effects.Delay.prototype = {
+	Pizzicato.Effects.Delay.prototype = Object.create(null, {
 	
-		applyToNode: function(node) {
+		/**
+		 * Applies the delay effect to a node and 
+		 * returns the master gain node;
+		 * @type {Function}
+		 */
+		applyToNode: {
 	
-			var currentNode = node;
+			writable: false,
 	
-			var dryGainNode = Pizzicato.context.createGain();
-			var wetGainNode = Pizzicato.context.createGain();
-			var masterGainNode = Pizzicato.context.createGain();
+			configurable: false,
 	
-			dryGainNode.gain.value = Pizzicato.Util.getDryLevel(this.options.mix);
-			wetGainNode.gain.value = Pizzicato.Util.getWetLevel(this.options.mix);
+			enumerable: true,
 	
-			node.connect(dryGainNode);
-	
-			for (var i = 0; i < this.options.repetitions; i++) {
-	
-				var delayNode = Pizzicato.context.createDelay();
-				delayNode.delayTime.value = this.options.time;
-	
-				var feedback = Pizzicato.context.createGain();
-				feedback.gain.value = 1 - (i * (1 / (this.options.repetitions)));
-	
-				currentNode.connect(delayNode);
-				delayNode.connect(feedback);
-				feedback.connect(wetGainNode);
-	
-				currentNode = delayNode;
+			value: function(node) {
+				node.connect(this.inputGainNode);
+				return this.outputGainNode;
 			}
-			
-			dryGainNode.connect(masterGainNode);
-			wetGainNode.connect(masterGainNode);
+		},
 	
-			return masterGainNode;
+		/**
+		 * Gets and sets the dry/wet mix.
+		 */
+		mix: {
+			get: function() {
+				return this.options.mix	;	
+			},
+	
+			set: function(mix) {
+				if (!Pz.Util.isNumber(mix) || mix < 0 || mix > 1)
+					return;
+	
+				this.options.mix = mix;
+				this.adjustMix();
+			}
+		},
+	
+		/**
+		 * Time between each delay loop
+		 */
+		time: {
+			get: function() {
+				return this.options.time;	
+			},
+	
+			set: function(time) {
+				if (!Pz.Util.isNumber(time))
+					return;
+	
+				this.options.time = time;
+				this.adjustTime();
+			}
+		},
+	
+		/**
+		 * Number of delayed sounds
+		 */
+		repetitions: {
+			get: function() {
+				return this.options.repetitions	;	
+			},
+	
+			set: function(repetitions) {
+				if (!Pz.Util.isNumber(repetitions))
+					return;
+	
+				this.options.repetitions = parseInt(repetitions, 10);
+				this.createDelayLoop();
+			}
+		},
+	
+		/**
+		 * Creates the loop with delay nodes. If a previous
+		 * delay loop exists, it disconnects it an re-creates it.
+		 */
+		createDelayLoop: {
+			writable: false,
+			configurable: false,
+			enumerable: true,
+			value: function() {
+				var i;
+	
+				if (this.delayLoop && this.delayLoop.length > 0) {
+					for (i = 0; i < this.delayLoop.length; i++) {
+						this.delayLoop[i].delay.disconnect();
+						this.delayLoop[i].feedback.disconnect();
+					}
+				}
+	
+				this.delayLoop = [];
+	
+				for (i = 0; i < this.repetitions; i++) {
+					
+					var delay = Pizzicato.context.createDelay();
+					var feedback = Pizzicato.context.createGain();
+					var parentNode = (i === 0) ? this.inputGainNode : this.delayLoop[i - 1].delay;
+					
+					delay.delayTime.value = this.time;
+					feedback.gain.value = 1 - (i * (1 / (this.repetitions)));
+	
+					delay.connect(feedback);
+					feedback.connect(this.wetGainNode);
+					parentNode.connect(delay);
+	
+					this.delayLoop.push({ delay: delay, feedback: feedback });
+				}				
+			}
+		},
+	
+		/**
+		 * Ensures the time of the delay nodes 
+		 * in the delay loop is correct
+		 */
+		adjustTime: {
+			writable: false,
+			configurable: false,
+			enumerable: true,
+			value: function() {
+	
+				for (var i = 0; i < this.delayLoop.length; i++)
+					this.delayLoop[i].delay.delayTime.value = this.time;
+			}
+		},
+	
+		/**
+		 * Ensures the dry/wet mix is correct.
+		 */
+		adjustMix: {
+			writable: false,
+			configurable: false,
+			enumerable: true,
+			value: function() {
+				this.dryGainNode.gain.value = Pizzicato.Util.getDryLevel(this.mix);
+				this.wetGainNode.gain.value = Pizzicato.Util.getWetLevel(this.mix);
+			}
 		}
 	
-	};
+	});
 
 	
 
--- tests/Effects/Delay.test.js
+++ tests/Effects/Delay.test.js
@@ -1,6 +1,6 @@
 describe('Effects.Delay', function() {
 
-	it('Should override default options', function(done) {
+	it('Should override default options', function() {
 		var options = {
 			repetitions: 10,
 			time: 0.6,
@@ -8,11 +8,54 @@ describe('Effects.Delay', function() {
 		};
 		var delay = new Pizzicato.Effects.Delay(options);
 
-		expect(delay.options.repetitions).toBe(options.repetitions);
-		expect(delay.options.time).toBe(options.time);
-		expect(delay.options.mix).toBe(options.mix);
+		expect(delay.repetitions).toBe(options.repetitions);
+		expect(delay.time).toBe(options.time);
+		expect(delay.mix).toBe(options.mix);
+	});
 
-		done();
-	}, 5000);
+
+	it('Should change the gain node values when changing the mix', function() {
+		var initialMix = 0;
+		var newMix = 0.5;
+
+		var delay = new Pizzicato.Effects.Delay({ mix: initialMix });
+
+		expect(delay.dryGainNode.gain.value).toBeCloseTo(Pz.Util.getDryLevel(0));
+		expect(delay.dryGainNode.gain.value).toBeCloseTo(Pz.Util.getDryLevel(0));
+
+		delay.mix = newMix;
+
+		expect(delay.dryGainNode.gain.value).toBeCloseTo(Pz.Util.getDryLevel(newMix));
+		expect(delay.dryGainNode.gain.value).toBeCloseTo(Pz.Util.getDryLevel(newMix));
+	});
+
+
+	it('Should change the delay node time values when changing the time', function() {
+		var initialTime = 0.3;
+		var newTime = 0.5;
+		var i;
+
+		var delay = new Pizzicato.Effects.Delay({ time: initialTime });
+
+		for (i = 0; i < delay.delayLoop.length; i++)
+			expect(delay.delayLoop[i].delay.delayTime.value).toBeCloseTo(initialTime);
+
+		delay.time = newTime;
+
+		for (i = 0; i < delay.delayLoop.length; i++)
+			expect(delay.delayLoop[i].delay.delayTime.value).toBeCloseTo(newTime);
+	});
+
+
+	it('Should change the times the delay is looped when changing the repetitions', function() {
+		var initialRepetitions = 10;
+		var newRepetitions = 5;
+
+		var delay = new Pizzicato.Effects.Delay({ repetitions: initialRepetitions });
+		expect(delay.delayLoop.length).toBe(10);
+
+		delay.repetitions = newRepetitions;
+		expect(delay.delayLoop.length).toBe(newRepetitions);
+	});
 
 });
