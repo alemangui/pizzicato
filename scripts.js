a9c54d17161cb518b92b86a6d9e2fe262787245c
Alejandro Mantecon Guillen <alemangui@gmail.com>
2015-04-12 22:34:42 +0200

    Additions to the website.

--- scripts.js
+++
@@ -1,7 +0,0 @@
-var clickSound = new Pizzicato.Sound('./audio/click.wav', function() {
-	console.log('clickSound loaded');
-});
-
-var birdSound = new Pizzicato.Sound('./audio/bird.wav', function() {
-	console.log('birdSound loaded');
-});
