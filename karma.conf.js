128d52d28a3eb50e0633739ff398411b1c7ca67f
Alejandro Mantecon Guillen <alemangui@gmail.com>
2015-06-21 13:28:25 +0200

    Travis CI Chrome issue fix

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,16 @@
 module.exports = function(config) {
 
-	config.set({
+	var cfg = {
  
 		browsers: ['Chrome'],
+
+		customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+    
 		frameworks: ['jasmine'],
 		files: [
 			'distr/Pizzicato.js',
@@ -14,7 +22,11 @@ module.exports = function(config) {
         included: false 
       }
 		]
+	};
 
-	});
+	if (process.env.TRAVIS)
+    cfg.browsers = ['Chrome_travis_ci'];
+
+	config.set(cfg);
 
 };
