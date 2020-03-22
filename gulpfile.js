f92a814fb0268ecba1c4d87dc168638b8db13a29
Alejandro MG <alemangui@gmail.com>
2020-03-22 12:20:05 +0100

    Updates Gulp to 4.0.2 (#129)

--- gulpfile.js
+++ gulpfile.js
@@ -6,39 +6,34 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var KarmaServer = require('karma').Server;
 
-
-gulp.task('hint', function() {
-	var stream = gulp.src('./src/**/*.js')
+function hint() {
+	return gulp.src('./src/**/*.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter('jshint-stylish'))
 		.pipe(jshint.reporter('fail'));
+};
 
-	return stream;
-});
-
-
-gulp.task('scripts', ['hint'], function() {
-	var stream = gulp.src('./src/Pizzicato.js')
+function scripts() {
+	return gulp.src('./src/Pizzicato.js')
 		.pipe(include())
 		.pipe(gulp.dest('./distr'))
 		.pipe(gulp.dest('./site'))
 		.pipe(rename('Pizzicato.min.js'))
 		.pipe(uglify())
 		.pipe(gulp.dest('./distr'));
+};
 
-	return stream;
-});
-
-
-gulp.task('test', ['scripts'], function(done) {
+function test(done) {
 	new KarmaServer({
 		configFile: __dirname + '/karma.conf.js',
 		singleRun: true
 	}, done).start();
-})
+};
 
-gulp.task('watch', ['scripts'], function() {
-	gulp.watch(['./src/**/*.js'], ['scripts']);
-});
+function watch() {
+	gulp.watch(['./src/**/*.js'], gulp.series(hint, scripts));
+};
 
-gulp.task('default', ['hint', 'scripts', 'test']);
+gulp.task('build', gulp.series(hint, scripts))
+gulp.task('test', gulp.series(hint, scripts, test))
+gulp.task('watch', gulp.series(hint, scripts, watch))
